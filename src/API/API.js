17e0a9b812f4b20a02e0d428419d793641ee6de6
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-04 16:10:07 -0500

    cleans up the css stylings.

--- src/API/API.js
+++ src/API/API.js
@@ -1,12 +1,11 @@
 async function getBooks() {
     try {
         const response = await fetch('http://localhost:4000/api/v1/books');
-        
         if (!response.ok) {
             throw new Error(`error status: ${response.status}`);
         }
-
         const data = await response.json();
+        console.log(data);
         return data;
     } catch (error) {
         console.error("Something went wrong:", error.message);
