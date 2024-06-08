976c738c263849f724732f15ffc6164d7062f19b
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-08 11:33:32 -0500

    completes about section.

--- src/About/About.js
+++ src/About/About.js
@@ -1,5 +1,6 @@
 import './About.css'
  function About () {
+    const email = "sims.lydia88@gmail.com"
     return(
         <>
             <div className='about-container'>
@@ -8,8 +9,17 @@ import './About.css'
                 </div>
                 <div className='about-contact'>
                     <h2>About Me</h2>
-                    <p>Summary</p>
-                    <h3>my contact info</h3>
+                    <p>
+                        Lydia Sims is a Front End Web Developer based out of of the humid Houston, TX. She has a background in Phsychology, Philosophy and Non Profit Organizational Management. 
+
+                        This application was a labor of love. Combining her passion for web developement, romance, and books. 
+
+                        If you would like to reach out to her for more information on the application she can be reached by email below.
+                    </p>
+                    <h3>
+                        My contact info: <a href={`mailto:${email}`} style={{ color: '#f4ebfe' }}>E-mail</a>
+                    </h3>
+
                 </div>
             </div>
         </>
